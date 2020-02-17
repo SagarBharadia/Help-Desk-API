58e47f3c9a690ec2bb444c4ddcffcefd56cd71cf
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-02-17 20:46:39 +0000

    Created table inserts for permission actions.

--- database/migrations/tenant_migrations/2020_01_01_000002_create_tenant_permission_actions_table.php
+++ database/migrations/tenant_migrations/2020_01_01_000002_create_tenant_permission_actions_table.php
@@ -16,9 +16,108 @@ class CreateTenantPermissionActionsTable extends Migration
         Schema::connection('tenant')->create('permission_actions', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('action');
+            $table->string('action')->unique();
             $table->timestamps();
         });
+
+        DB::table('permission_actions')->insert([
+          [
+            'name' => 'Create Users',
+            'action' => 'create-users'
+          ],
+          [
+            'name' => 'Read Users',
+            'action' => 'read-users'
+          ],
+          [
+            'name' => 'Update Users',
+            'action' => 'update-users'
+          ],
+          [
+            'name' => 'Deactivate Users',
+            'action' => 'deactivate-users'
+          ],
+          [
+            'name' => 'Create Roles',
+            'action' => 'create-roles'
+          ],
+          [
+            'name' => 'Read Roles',
+            'action' => 'read-roles'
+          ],
+          [
+            'name' => 'Update Roles',
+            'action' => 'update-roles'
+          ],
+          [
+            'name' => 'Delete Roles',
+            'action' => 'delete-roles'
+          ],
+          [
+            'name' => 'Assign Permissions to Roles',
+            'action' => 'assign-permissions-to-roles'
+          ],
+          [
+            'name' => 'Read Permissions for Roles',
+            'action' => 'read-permissions-for-roles'
+          ],
+          [
+            'name' => 'Update Permissions for Roles',
+            'action' => 'update-permissions-for-roles'
+          ],
+          [
+            'name' => 'Create Calls',
+            'action' => 'create-calls'
+          ],
+          [
+            'name' => 'Read Calls',
+            'action' => 'read-calls'
+          ],
+          [
+            'name' => 'Update Calls',
+            'action' => 'update-calls'
+          ],
+          [
+            'name' => 'Delete Calls',
+            'action' => 'delete-calls'
+          ],
+          [
+            'name' => 'Create Reports',
+            'action' => 'create-reports'
+          ],
+          [
+            'name' => 'Read Reports',
+            'action' => 'read-reports'
+          ],
+          [
+            'name' => 'Delete Reports',
+            'action' => 'delete-reports'
+          ],
+          [
+            'name' => 'Read User Logs',
+            'action' => 'read-user-logs'
+          ],
+          [
+            'name' => 'Create Clients',
+            'action' => 'create-clients'
+          ],
+          [
+            'name' => 'Read Clients',
+            'action' => 'read-clients'
+          ],
+          [
+            'name' => 'Update Clients',
+            'action' => 'update-clients'
+          ],
+          [
+            'name' => 'Delete Clients',
+            'action' => 'delete-clients'
+          ],
+          [
+            'name' => 'Search Previous Solved Logs',
+            'action' => 'search-previous-solved-logs'
+          ]
+        ]);
     }
 
     /**
