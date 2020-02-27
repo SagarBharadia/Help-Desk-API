83c5203ea7c08d9216747d4d3d64e2211679a723
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-02-27 23:14:02 +0000

    Created the function definitions and cleaned up the create function while also implementing the useractionlog.

--- app/Http/Controllers/TenantCallController.php
+++ app/Http/Controllers/TenantCallController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 
 use App\TenantCall;
+use App\TenantLogAction;
+use App\TenantUserActionLog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,6 +21,13 @@ class TenantCallController extends Controller
         //
     }
 
+  /**
+   * Create a new call.
+   *
+   * @param Request $request
+   * @return \Illuminate\Http\JsonResponse
+   * @throws \Illuminate\Validation\ValidationException
+   */
     public function create(Request $request)
     {
         // Validating the request
@@ -30,16 +39,48 @@ class TenantCallController extends Controller
             'tags' => 'required|string'
         ]);
 
-        // Creating the call using the TenantCall model
-        return TenantCall::create([
-            'receiver_id' => Auth::guard('tenant_api')->user()->id,
-            'current_analyst_id' => 0,
-            'client_id' => $request->get('client_id'),
-            'caller_name' => $request->get('caller_name'),
-            'name' => $request->get('name'),
-            'details' => $request->get('details'),
-            'tags' => $request->get('tags'),
-            'resolved' => 0
-        ]);
+        $call = new TenantCall();
+        $call->receiver_id = Auth::guard('tenant_api')->user()->id;
+        $call->current_analyst_id = 0;
+        $call->client_id = $request->get('client_id');
+        $call->caller_name = $request->get('caller_name');
+        $call->name = $request->get('name');
+        $call->details = $request->get('details');
+        $call->tags = $request->get('tags');
+        $call->resolved = 0;
+
+        $userActionLog = new TenantUserActionLog();
+        $userActionLog->user_id = Auth::guard('tenant_api')->user()->id;
+        $userActionLog->log_action_id = TenantLogAction::getIdOfAction('created-call');
+        $userActionLog->details = "Created call '".$call->name."'.";
+
+        if($call->save()) {
+          $response = response()->json([], 201);
+          if($userActionLog->log_action_id) $userActionLog->save();
+        } else {
+          $response = response()->json(['message' => 'Could not save call.'], 500);
+        }
+
+        return $response;
+    }
+
+    public function update(Request $request)
+    {
+
+    }
+
+    public function delete(Request $request)
+    {
+
+    }
+
+    public function getAll()
+    {
+
+    }
+
+    public function get($call_id)
+    {
+
     }
 }
