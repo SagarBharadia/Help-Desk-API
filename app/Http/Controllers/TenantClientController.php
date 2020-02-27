897bc9aa55fe0df82e74beae0ddf5575ff185f1d
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-02-27 14:38:26 +0000

    Created TenantClientController with required function definitions according to the routes.

---
+++ app/Http/Controllers/TenantClientController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TenantClientController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function create(Request $request)
+    {
+
+    }
+
+    public function update(Request $request)
+    {
+
+    }
+
+    public function delete(Request $request)
+    {
+
+    }
+
+    public function getAll()
+    {
+
+    }
+
+    public function get(int $client_id)
+    {
+
+    }
+}
