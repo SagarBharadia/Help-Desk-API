1e23a79a09985a4fea26c99145eb92edaace631e
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-03-04 23:09:01 +0000

    Update Authenticate.php
    Updated authenticate middleware to also check if the user is active.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -7,38 +7,42 @@ use Illuminate\Contracts\Auth\Factory as Auth;
 
 class Authenticate
 {
-    /**
-     * The authentication guard factory instance.
-     *
-     * @var \Illuminate\Contracts\Auth\Factory
-     */
-    protected $auth;
+  /**
+   * The authentication guard factory instance.
+   *
+   * @var \Illuminate\Contracts\Auth\Factory
+   */
+  protected $auth;
 
-    /**
-     * Create a new middleware instance.
-     *
-     * @param  \Illuminate\Contracts\Auth\Factory  $auth
-     * @return void
-     */
-    public function __construct(Auth $auth)
-    {
-        $this->auth = $auth;
+  /**
+   * Create a new middleware instance.
+   *
+   * @param \Illuminate\Contracts\Auth\Factory $auth
+   * @return void
+   */
+  public function __construct(Auth $auth)
+  {
+    $this->auth = $auth;
+  }
+
+  /**
+   * Handle an incoming request.
+   *
+   * @param \Illuminate\Http\Request $request
+   * @param \Closure $next
+   * @param string|null $guard
+   * @return mixed
+   */
+  public function handle($request, Closure $next, $guard = null)
+  {
+    if ($this->auth->guard($guard)->guest()) {
+      return response()->json(['message' => 'Unauthorized.'], 401);
     }
 
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
-    public function handle($request, Closure $next, $guard = null)
-    {
-        if ($this->auth->guard($guard)->guest()) {
-            return response()->json(['message' => 'Unauthorized.'], 401);
-        }
-
-        return $next($request);
+    if (!$this->auth->guard($guard)->user()->active) {
+      return response()->json(['message' => 'User inactive.'], 401);
     }
+
+    return $next($request);
+  }
 }
