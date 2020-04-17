a10a4eed0ee61c5cb10f5c1e703c5e4cc08bdd25
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-04-17 21:48:11 +0100

    Changed response status from 204 to 201.

--- app/Http/Controllers/TenantRoleController.php
+++ app/Http/Controllers/TenantRoleController.php
@@ -191,7 +191,7 @@ class TenantRoleController extends Controller
         }
         $role->delete();
         if ($userActionLog->log_action_id) $userActionLog->save();
-        $response = response()->json(['message' => 'Delete role.'], 204);
+        $response = response()->json(['message' => 'Delete role.'], 201);
       } else {
         $response = response()->json(['message' => 'Unable to delete role as users are still assigned it.'], 500);
       }
