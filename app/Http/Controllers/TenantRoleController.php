80956ca5cdc7b25173187faa97e125c1660d7a72
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-04-23 16:46:48 +0100

    Revised the response status codes and correctly applied again.

--- app/Http/Controllers/TenantRoleController.php
+++ app/Http/Controllers/TenantRoleController.php
@@ -153,7 +153,7 @@ class TenantRoleController extends Controller
         }
 
         if ($userActionLog->log_action_id) $userActionLog->save();
-        $response = response()->json(['message' => 'Updated role.'], 201);
+        $response = response()->json(['message' => 'Updated role.'], 200);
       } else {
         $response = response()->json(['message' => 'Couldn\'t save changes.'], 500);
       }
@@ -191,7 +191,7 @@ class TenantRoleController extends Controller
         }
         $role->delete();
         if ($userActionLog->log_action_id) $userActionLog->save();
-        $response = response()->json(['message' => 'Delete role.'], 201);
+        $response = response()->json(['message' => 'Delete role.'], 200);
       } else {
         $response = response()->json(['message' => 'Unable to delete role as users are still assigned it.'], 403);
       }
